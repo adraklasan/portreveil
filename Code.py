2ce2977822bdc41ead6616a02dc9fab41a570ac1
adraklasan <prathamkr26@gmail.com>
2024-01-17 13:19:21 +0530

    break error fix

--- Code.py
+++ Code.py
@@ -2,16 +2,16 @@ import socket #communicate w others in TCP & UDP
 import termcolor #to print statement in diff colors
 
 #func for scan multiple port
-def Sccan(targets, ports):
+def Scan(target, ports):
         print("\n" + "Starting scan for" + str(targets))    
         for port in range(1, ports):
-            Scan_port(targets, port)
+            Scan_port(target, port)
 
 #func for connect and check
-def Scan_port(ipadress, port):
+def Scan_port(ipaddress, port):
         try:    
             sock = socket.socket()
-            sock.connect(ipadress, port)
+            sock.connect(ipaddress, port)
             print("[+] Port OPEN " + str(port))
             sock.close()
         except:
@@ -22,6 +22,6 @@ ports = int(input("[?] Enetr how many Ports to Scan : "))
 if ',' in targets:
     print(termcolor.colored(("Scanning multiple ports:: "), 'green'))
     for ip_adr in targets.split(','):
-        Sccan(ip_adr.strip(' '), ports)
+        Scan(ip_adr.strip(' '), ports)
 else:
     Scan_port(targets, ports)
